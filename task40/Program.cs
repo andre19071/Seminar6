6336ef65052bad3be23c42ca88c9cc143d593f5f
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-24 14:37:35 +0700

    task40

--- task40/Program.cs
+++ task40/Program.cs
@@ -3,4 +3,18 @@
 //  с сторонами такой длины.Теорема о неравенстве треугольника: 
 //  каждая сторона треугольника меньше суммы двух других сторон.
 
+Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число C");
+int c = int.Parse(Console.ReadLine()!);
 
+if (a < b + c && b < c + a && c < b + a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
